c3545b63ea53a6e748bbf910db2bb9c434637a78
Sascha Majewsky <sascha.majewsky@pm.me>
2020-10-14 11:42:07 +0200

    Added a url generator python script for webscraping tripadvisor miniatur wunderland hamburg

---
+++ create-scraping-urls.py
@@ -0,0 +1,5 @@
+count = 5
+while (count < 15550):
+    output = "https://www.tripadvisor.de/Attraction_Review-g187331-d1027790-Reviews-or" + str(count) + "-Miniatur_Wunderland-Hamburg.html"
+    count += 5
+    print(output)
